e4923db65ac8495e79d095a818d948a19d1e093b
SAMIUL BASIR <samiulb25@gmail.com>
2024-01-26 05:44:59 +0600

    Class Mobile

---
+++ src/advanced/Mobile.java
@@ -0,0 +1,18 @@
+package advanced;
+
+public class Mobile {
+	
+	int price;
+	String name;
+	
+	public Mobile() {
+		
+		System.out.println("I am the constructor.");
+		
+	}
+	
+	static {
+		System.out.println("\nI am the static method.");
+	}
+
+}
