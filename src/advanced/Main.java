026094f184ca8c127f160a0443dc392fa5dc972e
SAMIUL BASIR <samiulb25@gmail.com>
2024-01-26 06:30:24 +0600

    Doing some update

--- src/advanced/Main.java
+++ src/advanced/Main.java
@@ -6,18 +6,20 @@ public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException {
 		
-		Mobile obj = new Mobile();
-		
-		Mobile.brand = "Redmi Note Series";
-		
-		obj.name = "Xiaomi";
-		
-		obj.price = 15000;
-		
-		Mobile.show(obj);
+//		Mobile obj = new Mobile();
+//		
+//		Mobile.brand = "Redmi Note Series";
+//		
+//		obj.name = "Xiaomi";
+//		
+//		obj.price = 15000;
+//		
+//		Mobile.show(obj);
 		
 		
 		//Class.forName("Mobile");
+		
+		
 		
 	}
 
