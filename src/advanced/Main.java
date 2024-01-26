466b3fd156f6e2c4467e324f1d3e9d66eeb6378b
SAMIUL BASIR <samiulb25@gmail.com>
2024-01-26 06:10:26 +0600

    Final Change

--- src/advanced/Main.java
+++ src/advanced/Main.java
@@ -1,10 +1,23 @@
 package advanced;
 
+import java.io.*;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
+		
+		Mobile obj = new Mobile();
+		
+		Mobile.brand = "Redmi Note Series";
+		
+		obj.name = "Xiaomi";
+		
+		obj.price = 15000;
+		
+		Mobile.show(obj);
 		
 		
+		//Class.forName("Mobile");
 		
 	}
 
--- src/advanced/Mobile.java
+++ src/advanced/Mobile.java
@@ -1,18 +1,26 @@
 package advanced;
 
 public class Mobile {
-	
+
 	int price;
 	String name;
-	
-	public Mobile() {
-		
-		System.out.println("I am the constructor.");
-		
-	}
-	
-	static {
-		System.out.println("\nI am the static method.");
+	static String brand;
+
+//	public Mobile() {
+//		
+//		System.out.println("I am the constructor.");
+//		
+//	}
+
+//	static {
+//		System.out.println("\nI am the static method.");
+//	}
+
+	public static void show(Mobile obj) {
+
+		System.out.println("The mobile name is: " + obj.name + "\nThe mobile brand name is: " + brand
+				+ "\nThe mobile price is: " + obj.price);
+
 	}
 
 }
